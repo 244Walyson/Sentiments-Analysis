3bbccdd4409967e7ec98e34bc35727f080225223
walyson moises <walysonmoises@gmail.com>
2024-10-31 23:24:09 -0300

    fine tunning bertimbau

---
+++ scripts/3_adicionar_rotulos.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from datasets import load_from_disk
+
+# Carregar os datasets tokenizados
+train_dataset = load_from_disk('data/train_dataset_tokenized')
+test_dataset = load_from_disk('data/test_dataset_tokenized')
+
+# Função para adicionar rótulos
+def add_labels(examples):
+    examples['labels'] = examples['polarity']  # Coluna com rótulos
+    return examples
+
+# Adicionar rótulos
+train_dataset = train_dataset.map(add_labels)
+test_dataset = test_dataset.map(add_labels)
+
+# Salvar datasets com rótulos
+train_dataset.save_to_disk('data/train_dataset_labeled')
+test_dataset.save_to_disk('data/test_dataset_labeled')
+
+print("Rótulos adicionados e datasets salvos com sucesso!")
